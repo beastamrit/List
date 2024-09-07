c22c1d7f52ed4b6b188833a7cc2258303749e566
beastamrit <mainamritjha@gmail.com>
2024-09-07 20:54:10 +0530

    palil

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ a=[]
 print(a)
 n=[1,2,3,4,5,6]
 print(n)
+b=[1,2,3]*3
+print(b)
+b=b[::-1]
+print(b)
+
+def m(w):
+    c=0
+    lst=[]
+    for word in w:
+        if len(word)>1 and word[0]== word[-1]:
+         c=c+1
+         return c
+count=m(["abc","1221"])
+print(count)
